10a22bc4c3e49d33a1f515f4c6de823524a1250b
Alexandra Watkins <xandra.watkins@gmail.com>
2025-01-22 11:49:27 -0600

    smil integration

--- smplx/body_models.py
+++ smplx/body_models.py
@@ -40,6 +40,7 @@ from .utils import (
 from .vertex_joint_selector import VertexJointSelector
 from collections import namedtuple
 
+
 TensorOutput = namedtuple('TensorOutput',
                           ['vertices', 'joints', 'betas', 'expression', 'global_orient', 'body_pose', 'left_hand_pose',
                            'right_hand_pose', 'jaw_pose', 'transl', 'full_pose'])
@@ -2293,12 +2294,410 @@ class FLAMELayer(FLAME):
                              full_pose=full_pose if return_full_pose else None)
         return output
 
+class SMIL(nn.Module):
+
+    NUM_JOINTS = 23
+    NUM_BODY_JOINTS = 23
+    NUM_BETAS = 20
+
+    def __init__(self, model_path, data_struct=None,
+                 create_betas=True,
+                 betas=None,
+                 create_global_orient=True,
+                 global_orient=None,
+                 create_body_pose=True,
+                 body_pose=None,
+                 create_transl=True,
+                 transl=None,
+                 dtype=torch.float32,
+                 batch_size=1,
+                 joint_mapper=None, gender='neutral',
+                 vertex_ids=None,
+                 **kwargs):
+        ''' SMIL model constructor
+
+            Parameters
+            ----------
+            model_path: str
+                The path to the folder or to the file where the model
+                parameters are stored
+            data_struct: Strct
+                A struct object. If given, then the parameters of the model are
+                read from the object. Otherwise, the model tries to read the
+                parameters from the given `model_path`. (default = None)
+            create_global_orient: bool, optional
+                Flag for creating a member variable for the global orientation
+                of the body. (default = True)
+            global_orient: torch.tensor, optional, Bx3
+                The default value for the global orientation variable.
+                (default = None)
+            create_body_pose: bool, optional
+                Flag for creating a member variable for the pose of the body.
+                (default = True)
+            body_pose: torch.tensor, optional, Bx(Body Joints * 3)
+                The default value for the body pose variable.
+                (default = None)
+            create_betas: bool, optional
+                Flag for creating a member variable for the shape space
+                (default = True).
+            betas: torch.tensor, optional, Bx20
+                The default value for the shape member variable.
+                (default = None)
+            create_transl: bool, optional
+                Flag for creating a member variable for the translation
+                of the body. (default = True)
+            transl: torch.tensor, optional, Bx3
+                The default value for the transl variable.
+                (default = None)
+            dtype: torch.dtype, optional
+                The data type for the created variables
+            batch_size: int, optional
+                The batch size used for creating the member variables
+            joint_mapper: object, optional
+                An object that re-maps the joints. Useful if one wants to
+                re-order the SMPL joints to some other convention (e.g. MSCOCO)
+                (default = None)
+            gender: str, optional
+                Which gender to load
+            vertex_ids: dict, optional
+                A dictionary containing the indices of the extra vertices that
+                will be selected
+        '''
+
+        self.gender = gender
+
+        if data_struct is None:
+            if osp.isdir(model_path):
+                model_fn = 'smil_web.pkl'
+                smil_path = os.path.join(model_path, model_fn)
+            else:
+                smil_path = model_path
+            assert osp.exists(smil_path), 'Path {} does not exist!'.format(
+                smil_path)
+
+            with open(smil_path, 'rb') as smil_file:
+                data_struct = Struct(**pickle.load(smil_file,
+                                                   encoding='latin1'))
+
+        super(SMIL, self).__init__()
+        self.batch_size = batch_size
+
+        if vertex_ids is None:
+            # SMIL and SMPL-H share the same topology, so any extra joints can
+            # be drawn from the same place
+            vertex_ids = VERTEX_IDS['smplh']
+
+        self.dtype = dtype
+
+        self.joint_mapper = joint_mapper
+
+        self.vertex_joint_selector = VertexJointSelector(
+            vertex_ids=vertex_ids, **kwargs)
+
+        self.faces = data_struct.f
+        self.register_buffer('faces_tensor',
+                             to_tensor(to_np(self.faces, dtype=np.int64),
+                                       dtype=torch.long))  
+
+        if create_betas:
+            if betas is None:
+                default_betas = torch.zeros([batch_size, self.NUM_BETAS],
+                                            dtype=dtype)
+            else:
+                if 'torch.Tensor' in str(type(betas)):
+                    default_betas = betas.clone().detach()
+                else:
+                    default_betas = torch.tensor(betas,
+                                                 dtype=dtype)
+
+            self.register_parameter('betas', nn.Parameter(default_betas,
+                                                          requires_grad=True))
+
+        # The tensor that contains the global rotation of the model
+        # It is separated from the pose of the joints in case we wish to
+        # optimize only over one of them
+        if create_global_orient:
+            if global_orient is None:
+                default_global_orient = torch.zeros([batch_size, 3],
+                                                    dtype=dtype)
+            else:
+                if 'torch.Tensor' in str(type(global_orient)):
+                    default_global_orient = global_orient.clone().detach()
+                else:
+                    default_global_orient = torch.tensor(global_orient,
+                                                         dtype=dtype)
+
+            global_orient = nn.Parameter(default_global_orient,
+                                         requires_grad=True)
+            self.register_parameter('global_orient', global_orient)
+
+        
+        if create_body_pose:
+            if body_pose is None:
+                default_body_pose = torch.zeros(
+                    [batch_size, self.NUM_BODY_JOINTS * 3], dtype=dtype)
+            else:
+                if 'torch.Tensor' in str(type(body_pose)):
+                    default_body_pose = body_pose.clone().detach()
+                else:
+                    default_body_pose = torch.tensor(body_pose,
+                                                     dtype=dtype)
+            self.register_parameter(
+                'body_pose',
+                nn.Parameter(default_body_pose, requires_grad=True))
+
+        if create_transl:
+            if transl is None:
+                default_transl = torch.zeros([batch_size, 3],
+                                             dtype=dtype,
+                                             requires_grad=True)
+            else:
+                default_transl = torch.tensor(transl, dtype=dtype)
+            self.register_parameter(
+                'transl',
+                nn.Parameter(default_transl, requires_grad=True))
+
+        # The vertices of the template model
+        self.register_buffer('v_template',
+                             to_tensor(to_np(data_struct.v_template),
+                                       dtype=dtype))
+
+        # The shape components
+        shapedirs = data_struct.shapedirs
+        # The shape components
+        self.register_buffer(
+            'shapedirs',
+            to_tensor(to_np(shapedirs), dtype=dtype))
+
+        j_regressor = to_tensor(to_np(
+            data_struct.J_regressor), dtype=dtype)
+        self.register_buffer('J_regressor', j_regressor)
+
+        # Pose blend shape basis: 6890 x 3 x 207, reshaped to 6890*3 x 207
+        num_pose_basis = data_struct.posedirs.shape[-1]
+        # 207 x 20670
+        posedirs = np.reshape(data_struct.posedirs, [-1, num_pose_basis]).T
+        self.register_buffer('posedirs',
+                             to_tensor(to_np(posedirs), dtype=dtype))
+
+        # indices of parents for each joints
+        parents = to_tensor(to_np(data_struct.kintree_table[0])).long()
+        parents[0] = -1
+        self.register_buffer('parents', parents)
+
+        self.register_buffer('lbs_weights',
+                             to_tensor(to_np(data_struct.weights), dtype=dtype))
+
+    def create_mean_pose(self, data_struct):
+        pass
+
+    @torch.no_grad()
+    def reset_params(self, **params_dict):
+        for param_name, param in self.named_parameters():
+            if param_name in params_dict:
+                param[:] = torch.tensor(params_dict[param_name])
+            else:
+                param.fill_(0)
+
+    def get_num_verts(self):
+        return self.v_template.shape[0]
+
+    def get_num_faces(self):
+        return self.faces.shape[0]
+
+    def extra_repr(self):
+        return 'Number of betas: {}'.format(self.NUM_BETAS)
+
+    def forward(self, betas=None, body_pose=None, global_orient=None,
+                transl=None, return_verts=True, return_full_pose=False, pose2rot=True,
+                **kwargs):
+        ''' Forward pass for the SMPL model
+
+            Parameters
+            ----------
+            global_orient: torch.tensor, optional, shape Bx3
+                If given, ignore the member variable and use it as the global
+                rotation of the body. Useful if someone wishes to predicts this
+                with an external model. (default=None)
+            betas: torch.tensor, optional, shape Bx10
+                If given, ignore the member variable `betas` and use it
+                instead. For example, it can used if shape parameters
+                `betas` are predicted from some external model.
+                (default=None)
+            body_pose: torch.tensor, optional, shape Bx(J*3)
+                If given, ignore the member variable `body_pose` and use it
+                instead. For example, it can used if someone predicts the
+                pose of the body joints are predicted from some external model.
+                It should be a tensor that contains joint rotations in
+                axis-angle format. (default=None)
+            transl: torch.tensor, optional, shape Bx3
+                If given, ignore the member variable `transl` and use it
+                instead. For example, it can used if the translation
+                `transl` is predicted from some external model.
+                (default=None)
+            return_verts: bool, optional
+                Return the vertices. (default=True)
+            return_full_pose: bool, optional
+                Returns the full axis-angle pose vector (default=False)
+
+            Returns
+            -------
+        '''
+        # If no shape and pose parameters are passed along, then use the
+        # ones from the module
+        global_orient = (global_orient if global_orient is not None else
+                         self.global_orient)
+        body_pose = body_pose if body_pose is not None else self.body_pose
+
+        betas = betas if betas is not None else self.betas
+
+        apply_trans = transl is not None or hasattr(self, 'transl')
+        if transl is None and hasattr(self, 'transl'):
+            transl = self.transl
+
+        full_pose = torch.cat([global_orient, body_pose], dim=1)
+
+        batch_size = max(betas.shape[0], global_orient.shape[0],
+                         body_pose.shape[0])
+
+        if betas.shape[0] != batch_size:
+            num_repeats = int(batch_size / betas.shape[0])
+            betas = betas.expand(num_repeats, -1)
+
+        vertices, joints = lbs(betas, full_pose, self.v_template,
+                               self.shapedirs, self.posedirs,
+                               self.J_regressor, self.parents,
+                               self.lbs_weights, pose2rot=pose2rot, dtype=self.dtype)
+
+        joints = self.vertex_joint_selector(vertices, joints)
+        # Map the joints to the current dataset
+        if self.joint_mapper is not None:
+            joints = self.joint_mapper(joints)
+
+        if apply_trans:
+            joints += transl.unsqueeze(dim=1)
+            vertices += transl.unsqueeze(dim=1)
+
+        output = SMPLOutput(vertices=vertices if return_verts else None,
+                             global_orient=global_orient,
+                             body_pose=body_pose,
+                             joints=joints,
+                             betas=betas,
+                             full_pose=full_pose if return_full_pose else None)
+
+        return output
+    
+class SMILLayer(SMIL):
+    def __init__(
+        self,
+        *args,
+        **kwargs
+    ) -> None:
+        # Just create a SMPL module without any member variables
+        super(SMILLayer, self).__init__(
+            create_body_pose=False,
+            create_betas=False,
+            create_global_orient=False,
+            create_transl=False,
+            *args,
+            **kwargs,
+        )
+
+    def forward(
+        self,
+        betas: Optional[Tensor] = None,
+        body_pose: Optional[Tensor] = None,
+        global_orient: Optional[Tensor] = None,
+        transl: Optional[Tensor] = None,
+        return_verts=True,
+        return_full_pose: bool = False,
+        pose2rot: bool = True,
+        **kwargs
+    ) -> SMPLOutput:
+        ''' Forward pass for the SMPL model
+
+            Parameters
+            ----------
+            global_orient: torch.tensor, optional, shape Bx3x3
+                Global rotation of the body.  Useful if someone wishes to
+                predicts this with an external model. It is expected to be in
+                rotation matrix format.  (default=None)
+            betas: torch.tensor, optional, shape BxN_b
+                Shape parameters. For example, it can used if shape parameters
+                `betas` are predicted from some external model.
+                (default=None)
+            body_pose: torch.tensor, optional, shape BxJx3x3
+                Body pose. For example, it can used if someone predicts the
+                pose of the body joints are predicted from some external model.
+                It should be a tensor that contains joint rotations in
+                rotation matrix format. (default=None)
+            transl: torch.tensor, optional, shape Bx3
+                Translation vector of the body.
+                For example, it can used if the translation
+                `transl` is predicted from some external model.
+                (default=None)
+            return_verts: bool, optional
+                Return the vertices. (default=True)
+            return_full_pose: bool, optional
+                Returns the full axis-angle pose vector (default=False)
+
+            Returns
+            -------
+        '''
+        model_vars = [betas, global_orient, body_pose, transl]
+        batch_size = 1
+        for var in model_vars:
+            if var is None:
+                continue
+            batch_size = max(batch_size, len(var))
+        device, dtype = self.shapedirs.device, self.shapedirs.dtype
+        if global_orient is None:
+            global_orient = torch.eye(3, device=device, dtype=dtype).view(
+                1, 1, 3, 3).expand(batch_size, -1, -1, -1).contiguous()
+        if body_pose is None:
+            body_pose = torch.eye(3, device=device, dtype=dtype).view(
+                1, 1, 3, 3).expand(
+                    batch_size, self.NUM_BODY_JOINTS, -1, -1).contiguous()
+        if betas is None:
+            betas = torch.zeros([batch_size, self.num_betas],
+                                dtype=dtype, device=device)
+        if transl is None:
+            transl = torch.zeros([batch_size, 3], dtype=dtype, device=device)
+        full_pose = torch.cat(
+            [global_orient.reshape(-1, 1, 3, 3),
+             body_pose.reshape(-1, self.NUM_BODY_JOINTS, 3, 3)],
+            dim=1)
+
+        vertices, joints = lbs(betas, full_pose, self.v_template,
+                               self.shapedirs, self.posedirs,
+                               self.J_regressor, self.parents,
+                               self.lbs_weights,
+                               pose2rot=pose2rot)
+
+        joints = self.vertex_joint_selector(vertices, joints)
+        # Map the joints to the current dataset
+        if self.joint_mapper is not None:
+            joints = self.joint_mapper(joints)
+
+        if transl is not None:
+            joints += transl.unsqueeze(dim=1)
+            vertices += transl.unsqueeze(dim=1)
+
+        output = SMPLOutput(vertices=vertices if return_verts else None,
+                            global_orient=global_orient,
+                            body_pose=body_pose,
+                            joints=joints,
+                            betas=betas,
+                            full_pose=full_pose if return_full_pose else None)
+
+        return output
+
 
 def build_layer(
     model_path: str,
     model_type: str = 'smpl',
     **kwargs
-) -> Union[SMPLLayer, SMPLHLayer, SMPLXLayer, MANOLayer, FLAMELayer]:
+) -> Union[SMPLLayer, SMPLHLayer, SMPLXLayer, SMILLayer,MANOLayer, FLAMELayer]:
     ''' Method for creating a model from a path and a model type
 
         Parameters
@@ -2319,6 +2718,8 @@ def build_layer(
                 |-- SMPLX_FEMALE
                 |-- SMPLX_NEUTRAL
                 |-- SMPLX_MALE
+            |-- smil
+                |-- SMIL_WEB
             |-- mano
                 |-- MANO RIGHT
                 |-- MANO LEFT
@@ -2326,6 +2727,7 @@ def build_layer(
                 |-- FLAME_FEMALE
                 |-- FLAME_MALE
                 |-- FLAME_NEUTRAL
+
 
         model_type: str, optional
             When model_path is a folder, then this parameter specifies  the
@@ -2354,19 +2756,23 @@ def build_layer(
         return SMPLHLayer(model_path, **kwargs)
     elif model_type.lower() == 'smplx':
         return SMPLXLayer(model_path, **kwargs)
+    elif model_type.lower() == 'smil':
+        return SMILLayer(model_path,**kwargs)
     elif 'mano' in model_type.lower():
         return MANOLayer(model_path, **kwargs)
     elif 'flame' in model_type.lower():
         return FLAMELayer(model_path, **kwargs)
     else:
         raise ValueError(f'Unknown model type {model_type}, exiting!')
+    
+
 
 
 def create(
     model_path: str,
     model_type: str = 'smpl',
     **kwargs
-) -> Union[SMPL, SMPLH, SMPLX, MANO, FLAME]:
+) -> Union[SMPL, SMPLH, SMPLX, SMIL, MANO, FLAME]:
     ''' Method for creating a model from a path and a model type
 
         Parameters
@@ -2387,6 +2793,8 @@ def create(
                 |-- SMPLX_FEMALE
                 |-- SMPLX_NEUTRAL
                 |-- SMPLX_MALE
+            |-- smil
+                |-- SMIL_WEB
             |-- mano
                 |-- MANO RIGHT
                 |-- MANO LEFT
@@ -2419,6 +2827,8 @@ def create(
         return SMPLH(model_path, **kwargs)
     elif model_type.lower() == 'smplx':
         return SMPLX(model_path, **kwargs)
+    elif model_type.lower() == 'smil':
+        return SMIL(model_path, **kwargs)
     elif 'mano' in model_type.lower():
         return MANO(model_path, **kwargs)
     elif 'flame' in model_type.lower():
