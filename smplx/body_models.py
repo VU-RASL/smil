4bc1b8b0c22635a4e8d51cb567794249d50fdecf
Alexandra Watkins <xandra.watkins@gmail.com>
2025-01-22 12:47:45 -0600

    updated to match pypi smplx code

--- smplx/body_models.py
+++ smplx/body_models.py
@@ -38,12 +38,6 @@ from .utils import (
     FLAMEOutput,
     find_joint_kin_chain)
 from .vertex_joint_selector import VertexJointSelector
-from collections import namedtuple
-
-
-TensorOutput = namedtuple('TensorOutput',
-                          ['vertices', 'joints', 'betas', 'expression', 'global_orient', 'body_pose', 'left_hand_pose',
-                           'right_hand_pose', 'jaw_pose', 'transl', 'full_pose'])
 
 
 class SMPL(nn.Module):
@@ -54,7 +48,7 @@ class SMPL(nn.Module):
 
     def __init__(
         self, model_path: str,
-        kid_template_path: str = '',
+	kid_template_path: str = '',
         data_struct: Optional[Struct] = None,
         create_betas: bool = True,
         betas: Optional[Tensor] = None,
@@ -148,20 +142,16 @@ class SMPL(nn.Module):
         shapedirs = data_struct.shapedirs
         if (shapedirs.shape[-1] < self.SHAPE_SPACE_DIM):
             print(f'WARNING: You are using a {self.name()} model, with only'
-                  f' {shapedirs.shape[-1]} shape coefficients.\n'
-                  f'num_betas={num_betas}, shapedirs.shape={shapedirs.shape}, '
-                  f'self.SHAPE_SPACE_DIM={self.SHAPE_SPACE_DIM}')
-            num_betas = min(num_betas, shapedirs.shape[-1])
+                  ' 10 shape coefficients.')
+            num_betas = min(num_betas, 10)
         else:
             num_betas = min(num_betas, self.SHAPE_SPACE_DIM)
 
-        if self.age == 'kid':
+        if self.age=='kid':
             v_template_smil = np.load(kid_template_path)
             v_template_smil -= np.mean(v_template_smil, axis=0)
-            v_template_diff = np.expand_dims(
-                v_template_smil - data_struct.v_template, axis=2)
-            shapedirs = np.concatenate(
-                (shapedirs[:, :, :num_betas], v_template_diff), axis=2)
+            v_template_diff = np.expand_dims(v_template_smil - data_struct.v_template, axis=2)
+            shapedirs = np.concatenate((shapedirs[:, :, :num_betas], v_template_diff), axis=2)
             num_betas = num_betas + 1
 
         self._num_betas = num_betas
@@ -523,7 +513,6 @@ class SMPLH(SMPL):
         right_hand_pose: Optional[Tensor] = None,
         use_pca: bool = True,
         num_pca_comps: int = 6,
-        num_betas=16,
         flat_hand_mean: bool = False,
         batch_size: int = 1,
         gender: str = 'neutral',
@@ -602,7 +591,6 @@ class SMPLH(SMPL):
             model_path=model_path,
             kid_template_path=kid_template_path,
             data_struct=data_struct,
-            num_betas=num_betas,
             batch_size=batch_size, vertex_ids=vertex_ids, gender=gender, age=age,
             use_compressed=use_compressed, dtype=dtype, ext=ext, **kwargs)
 
@@ -910,7 +898,7 @@ class SMPLX(SMPLH):
 
     def __init__(
         self, model_path: str,
-        kid_template_path: str = '',
+	kid_template_path: str = '',
         num_expression_coeffs: int = 10,
         create_expression: bool = True,
         expression: Optional[Tensor] = None,
@@ -1236,7 +1224,6 @@ class SMPLX(SMPLH):
         scale = int(batch_size / betas.shape[0])
         if scale > 1:
             betas = betas.expand(scale, -1)
-            expression = expression.expand(scale, -1)
         shape_components = torch.cat([betas, expression], dim=-1)
 
         shapedirs = torch.cat([self.shapedirs, self.expr_dirs], dim=-1)
@@ -1286,20 +1273,17 @@ class SMPLX(SMPLH):
         v_shaped = None
         if return_shaped:
             v_shaped = self.v_template + blend_shapes(betas, self.shapedirs)
-        else:
-            v_shaped = Tensor(0)
         output = SMPLXOutput(vertices=vertices if return_verts else None,
-                              joints=joints,
-                              betas=betas,
-                              expression=expression,
-                              global_orient=global_orient,
-                              transl=transl,
-                              body_pose=body_pose,
-                              left_hand_pose=left_hand_pose,
-                              right_hand_pose=right_hand_pose,
-                              jaw_pose=jaw_pose,
-                              v_shaped=v_shaped,
-                              full_pose=full_pose if return_full_pose else None)
+                             joints=joints,
+                             betas=betas,
+                             expression=expression,
+                             global_orient=global_orient,
+                             body_pose=body_pose,
+                             left_hand_pose=left_hand_pose,
+                             right_hand_pose=right_hand_pose,
+                             jaw_pose=jaw_pose,
+                             v_shaped=v_shaped,
+                             full_pose=full_pose if return_full_pose else None)
         return output
 
 
@@ -1337,9 +1321,9 @@ class SMPLXLayer(SMPLX):
         leye_pose: Optional[Tensor] = None,
         reye_pose: Optional[Tensor] = None,
         return_verts: bool = True,
-        return_full_pose: bool = True,
+        return_full_pose: bool = False,
         **kwargs
-    ) -> TensorOutput:
+    ) -> SMPLXOutput:
         '''
         Forward pass for the SMPLX model
 
@@ -1488,18 +1472,17 @@ class SMPLXLayer(SMPLX):
             joints += transl.unsqueeze(dim=1)
             vertices += transl.unsqueeze(dim=1)
 
-        output = TensorOutput(vertices=vertices if return_verts else Tensor(0),
-                              joints=joints,
-                              betas=betas,
-                              expression=expression,
-                              global_orient=global_orient,
-                              body_pose=body_pose,
-                              left_hand_pose=left_hand_pose,
-                              right_hand_pose=right_hand_pose,
-                              jaw_pose=jaw_pose,
-                              transl=transl if transl != None else Tensor(0),
-                              full_pose=full_pose if return_full_pose else Tensor(0))
-
+        output = SMPLXOutput(vertices=vertices if return_verts else None,
+                             joints=joints,
+                             betas=betas,
+                             expression=expression,
+                             global_orient=global_orient,
+                             body_pose=body_pose,
+                             left_hand_pose=left_hand_pose,
+                             right_hand_pose=right_hand_pose,
+                             jaw_pose=jaw_pose,
+                             transl=transl,
+                             full_pose=full_pose if return_full_pose else None)
         return output
 
 
@@ -2090,7 +2073,6 @@ class FLAME(SMPL):
         scale = int(batch_size / betas.shape[0])
         if scale > 1:
             betas = betas.expand(scale, -1)
-            expression = expression.expand(scale, -1)
         shape_components = torch.cat([betas, expression], dim=-1)
         shapedirs = torch.cat([self.shapedirs, self.expr_dirs], dim=-1)
 
@@ -2293,6 +2275,7 @@ class FLAMELayer(FLAME):
                              jaw_pose=jaw_pose,
                              full_pose=full_pose if return_full_pose else None)
         return output
+
 
 class SMIL(nn.Module):
 
